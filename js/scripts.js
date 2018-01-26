51c9ec1758db5a1ece9678b9b84998b693746c25
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-01-26 11:01:26 -0800

    Create UI logic when user submits form. Also add size in the confirmation container in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var crustCost = [5.00, 5.50, 6.00];
 var sizeCost = [1.00, 2.00, 3.00];
 var toppingCost = [1.25, 1.00, 0.50, 1.50, 2.00, 1.50, 1.00, 1.00, 1.50, 1.00];
-var toppingName = ["Pepperoni", "Mushrooms","Onions","Sausage","Bacon","Extra Cheese", "Black Olives", "Green Peppers", "Pineapple", "Spinach"];
+var crustName = ['Thin Crust', 'Brooklyn Style', 'Handmade Pan Pizza Crust']
+var sizeName = ['Small (12")', 'Medium (14")', 'Large (14")']
+var toppingName = ["Pepperoni", " Mushrooms"," Onions"," Sausage"," Bacon"," Extra Cheese", " Black Olives", " Green Peppers", " Pineapple", " Spinach"];
 
 function Pizza(crust, size, toppings){
   this.crust = crust;
@@ -9,11 +11,36 @@ function Pizza(crust, size, toppings){
   this.toppings = toppings;
 };
 
-Pizza.prototype.toppingsCost = function(){
-  var total = 0;
+// Pizza.prototype.toppingsCost = function(){
+//   var total = 0;
+//
+//   .forEach(function(topping){
+//     pizzaCost += parseInt(topping);
+//   });
+//   return total;
+// }
 
-  .forEach(function(topping){
-    pizzaCost += parseInt(topping);
+
+$(document).ready(function(){
+  var indexCrust = 0;
+  var indexSize = 0;
+  var indexToppings = [];
+
+  $("#creationForm").submit(function(event){
+    event.preventDefault();
+    indexCrust = parseInt($("input:radio[name=typeCrust]:checked").val());
+    indexSize = parseInt($("input:radio[name=sizeCrust]:checked").val());
+    var tempNameTops = [];
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var tempTopping = parseInt($(this).val());
+      indexToppings.push(tempTopping);
+    });
+    indexToppings.forEach(function(index){
+      tempNameTops.push(toppingName[index]);
+    })
+    var pizza = new Pizza(crustName[indexCrust], sizeName[indexSize], tempNameTops);
+    $("#crustDisplay").text(pizza.crust);
+    $("#sizeDisplay").text(pizza.size);
+    $("#toppingsDisplay").text(pizza.toppings);
   });
-  return total;
-}
+});
